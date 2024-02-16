b46f7c1c8c5cba280b78847c3d2b043a9921a9bf
TsilavinaDevJS <tsilavinaandriamahafaly01@gmail.com>
2024-02-16 10:55:44 +0300

    feat:add a transition visibility navbar in view

--- client/src/layout/navigation/NavBar.tsx
+++ client/src/layout/navigation/NavBar.tsx
@@ -1,14 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MenuMobile from './mobile/MenuMobile';
 import MenuDesktop from './desktop/MenuDesktop';
 
 const NavBar = () => {
+  const previousPosition = useRef(0);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [scrollDirection, setScrollDirection] = useState<string>('down');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let currentPosition =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (currentPosition > previousPosition.current) {
+        setScrollDirection('down');
+      } else {
+        setScrollDirection('up');
+      }
+
+      previousPosition.current = currentPosition;
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="border-b">
-      <MenuDesktop setIsOpen={setIsOpen} />
-      <MenuMobile isOpen={isOpen} setIsOpen={setIsOpen} />
+    <header>
+      <div
+        className={`fixed w-full top-0 left-0 transition-opacity duration-700 z-50 ${scrollDirection === 'up' ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <MenuDesktop setIsOpen={setIsOpen} />
+        <MenuMobile isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
     </header>
   );
 };
